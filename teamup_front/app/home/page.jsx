4605d5640c33ca105506798e95fa9c40d5510c25
Chanatt <66070035@kmitl.ac.th>
2025-10-30 11:11:28 +0700

    Update page.jsx

--- teamup_front/app/home/page.jsx
+++ teamup_front/app/home/page.jsx
@@ -4,7 +4,7 @@
 import { useEffect, useState } from "react";
 import { Calendar, MapPin } from "lucide-react";
 import { useRouter } from "next/navigation";
-import Header from "../components/header.jsx";
+import Header from "../component/header.jsx"
 
 const API = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3100";
 
